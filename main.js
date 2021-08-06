0abee76d7d25143648f3ef29dbafa8ce0cd36fab
CODERBLOXx <78531418+CODERBLOXx@users.noreply.github.com>
2021-08-06 17:43:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+objects = [];
+
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
